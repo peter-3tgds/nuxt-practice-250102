3103b9fda9684cf87c746f4c7df18858f7ce69d3
peter-3tgds <peter.chen@3tgds.com>
2024-06-28 15:15:30 +0800

    install tailwindcss tailwindcss-prettier

--- app.vue
+++ app.vue
@@ -1,12 +1,17 @@
 <template>
-  <div>
-    <h1 class="title">Hello Nuxt!!</h1>
-    <p>{{ name }}</p>
+  <div class="container">
+    <h1 class="text-3xl font-bold text-primary">Hello Nuxt!!</h1>
+    <p class="text-2xl text-secondary">{{ name }}</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, magni?</p>
+    <div class="flex space-x-1">
+      <button type="button" class="btn btn-primary">按鈕</button>
+      <button type="button" class="btn btn-secondary">按鈕</button>
+    </div>
   </div>
 </template>
 
 <script setup>
-const name = 'peter'
+const name = ref('peter')
 </script>
 
 <style lang="scss" scoped>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#BB371B',
+        secondary: '#9FBB1B'
+      }
+    },
+    container: {
+      center: true
+    }
+  },
+  plugins: []
+}
