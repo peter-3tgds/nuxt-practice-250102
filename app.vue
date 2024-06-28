822ccdcf5481464aa9c09ef66b310b950bfbc044
peter-3tgds <peter.chen@3tgds.com>
2024-06-28 11:52:00 +0800

    init

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1 class="title">Hello Nuxt!!</h1>
+    <p>{{ count }}</p>
+    <button type="button" @click="() => count += 1">增加</button>
+  </div>
+</template>
+
+<script setup>
+
+const count = ref(10)
+</script>
+
+<style lang="scss" scoped>
+.title {
+  font-size: 100px;
+  color: blue;
+  line-height: 1.5;
+}
+</style>
