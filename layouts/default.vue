50fe532f0b48921a484bea372543e0c71637c2fe
peter-3tgds <peter.chen@3tgds.com>
2024-07-01 11:21:00 +0800

    feat Layout: 添加多個 slot> header, footer, default

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,8 @@
     <p class="px-6 py-2 text-2xl text-sky-600">
       這個藍色的背景是預設的布局，預計給全部的頁面做使用
     </p>
+    <slot name="header" />
     <slot />
+    <slot name="footer" />
   </div>
 </template>
