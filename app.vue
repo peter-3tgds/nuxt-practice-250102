fb01181c95fac47d1d72be5f5c60373c76f7815c
peter-3tgds <peter.chen@3tgds.com>
2024-07-01 11:20:08 +0800

    feat Layout: 添加 slot

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-    <NuxtLayout />
+    <p class="my-2 px-2 text-2xl text-gray-700">這裡是最外層 app.vue</p>
+    <div>
+      <NuxtLayout>
+        <p class="px-12 py-6 text-xl text-rose-500">
+          被 NuxtLayout 包裹的元件將會放置到 Layout 的預設 slot 插槽中
+        </p>
+      </NuxtLayout>
+    </div>
   </div>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,5 +3,6 @@
     <p class="px-6 py-2 text-2xl text-sky-600">
       這個藍色的背景是預設的布局，預計給全部的頁面做使用
     </p>
+    <slot />
   </div>
 </template>
