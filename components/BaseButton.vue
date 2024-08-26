4eff20507db3574050912244d0e12cd9dc4085f6
peter-3tgds <peter.chen@3tgds.com>
2024-08-26 11:42:30 +0800

    feat: Lazy前綴>延遲載入元件

---
+++ components/BaseButton.vue
@@ -0,0 +1,3 @@
+<template>
+  <button type="button" class="btn btn-primary">註冊</button>
+</template>
--- pages/component2.vue
+++ pages/component2.vue
@@ -7,8 +7,17 @@
           <p class="border border-black text-4xl font-bold text-red-500">伺服器端渲染，載入中...</p>
         </template>
       </ClientOnly>
+      <div class="flex">
+        <input id="show" v-model="show" type="checkbox" name="show" />
+        <label for="show" class="ml-2 text-2xl">延遲載入元件</label>
+      </div>
+      <div>
+        <LazyBaseButton v-if="show" />
+      </div>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const show = ref(false)
+</script>
