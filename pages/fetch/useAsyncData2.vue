439a3955005f2b4ba4010272eed4bcd2af52c3cb
peter-3tgds <peter.chen@3tgds.com>
2025-01-06 15:56:19 +0800

    feat: useAsyncData 加入 transform method 格式化錯誤資料

--- pages/fetch/useAsyncData2.vue
+++ pages/fetch/useAsyncData2.vue
@@ -10,10 +10,43 @@
 </template>
 
 <script setup>
-const { data, error, status, refresh } = await useAsyncData('index', async () => {
-  // const apis = [$fetch('https://randomuser.me/api/'), $fetch('https://randomuser.me/api/')] // 2正確 API
-  const apis = [$fetch('https://randomuser.me/api/'), $fetch('https://randomuser123.me/api/')] // 1正確 1錯誤 API
-  const res = await Promise.allSettled(apis)
-  return res
-})
+import { FetchError } from 'ofetch'
+
+// 定義轉換函數
+function transformPromiseResults(results) {
+  return results.map((response) => {
+    // 錯誤的 ajax, 轉換回傳格式
+    if (response.status === 'rejected' && response.reason instanceof FetchError) {
+      // 將 FetchError 物件轉為字串，避免序列化問題
+      return {
+        ...response,
+        reason: response.reason.toString()
+      }
+    }
+
+    // 正確的 ajax 不用轉換
+    return response
+  })
+}
+
+// 使用 useAsyncData 搭配 Promise.allSettled
+const { data, error, status, refresh } = await useAsyncData(
+  'index',
+  async () => {
+    // 模擬一個成功、一個失敗的 API 請求
+    const apis = [$fetch('https://randomuser.me/api/'), $fetch('https://randomuser123.me/api/')]
+    const res = await Promise.allSettled(apis)
+    return res
+  },
+  {
+    transform: transformPromiseResults // 使用轉換函數處理結果
+  }
+)
+
+// 解法一：
+// 原本 return res
+// 改成 return JSON.parse(JSON.stringify(res));
+
+// 解法二：
+// transform: transformPromiseResults // 使用轉換函數處理結果
 </script>
